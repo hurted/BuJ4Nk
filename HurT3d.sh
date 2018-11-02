e62243aeeb1778175d6612da014cccfbc3f3c94c
hurted <44683565+hurted@users.noreply.github.com>
2018-11-02 15:19:07 +0700

    Create HurT3d.sh

---
+++ HurT3d.sh
@@ -0,0 +1,153 @@
+#!/system/xbin/bash
+#Gua gk Recode Yah Bngst
+#Kalo Mau Recode yah gpp silahkan aja
+
+clear
+blue='\033[31;1m'
+cyan='\033[32;1m'
+green='\033[33;1m'
+okegreen='\033[34;1m'
+purple='\033[35;1m'
+red='\033[36;1m'
+yellow='\033[37;1m'
+white='\033[38;1m'
+sleep 1
+toilet -f big -F gay "To0ls" | lolcat
+toilet -f big -F gay "Mr.HurT3d"
+echo""
+echo "\033[34;1mNama gua : Firzii"
+echo "\033[32;1mTeam : Indonesian Sad Cyber"
+echo "\033[35;1mStatus : Jomblo :v"
+echo "\033[31;1mContac me : 6281229312012
+echo "\033[38;1m {------------------------------}"
+sleep 1
+echo ""
+echo "\033[32;1mLangsung Aja di Pilih gratis kok"
+echo "\033[36;1m"
+echo "{______________________________________}
+echo $red "1.> Auto Like Facebook"
+echo "{======================================}
+echo $yellow "2.> Facebook_Auto Reaction"
+echo "{======================================}
+echo $green "3.> Bot Komen Facebook"
+echo "{======================================}
+echo $blue "4.> Full Bot Facebokk"
+echo "{======================================}
+echo $purple "5.> spam Sms Matahari Mall"
+echo "{======================================}
+echo $red "6.> Spam Sms HooqTV"
+echo "{======================================}
+echo $white "7.> spam Sms Kfc"
+echo "{======================================}
+echo $yellow "8.> spam Sms Whiskas"
+echo "{======================================}
+echo $cyan "9.> Spam Akun Gmail"
+echo "{======================================}
+echo $green "10.> main Tebak tebakan biar gk bt:v"
+echo "{======================================}
+echo $red "0.> Keluar"
+echo "\033[33;1m"
+read -p "[root@Mr.HurT3d:~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Mr.HurT3d" | lolcat
+cd module
+php autolike.php
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "Mr.HurT3d"
+cd module
+python2 auto_reaction.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet "Mr.HurT3d" | lolcat
+cd module
+python2 botkomen.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet "Mr.HurT3d"
+cd module
+python2 facebook.py
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+figlet "Mr.HurT3d" | lolcat
+cd module
+php mataharimall.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet "Mr.HurT3d"
+cd module
+php hooq.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+figlet "Mr.HurT3d" | lolcat
+cd module
+php kfc.php
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+toilet "Mr.HurT3d"
+cd module
+php whiskas.php
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+figlet "Mr.HurT3d" | lolcat
+cd module
+python2 SpamMail.py
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then 
+clear
+cowsay -f kiss.cow "Mr.HurT3d" | lolcat
+figlet "Mr.HurT3d" | lolcat
+sleep 1
+cd module
+python2 r.py
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mGua kasih Gratis Bujank"
+sleep 1
+echo "\033[33;1mGua Firzii"
+sleep 1
+echo " Gua Mana Ada Teman"
+sleep 1
+echo "I Love You Cewe Yg Kucintai"
+sleep 1
+echo "Hackers Itu Apa?;)"
+sleep 1
+echo "Ketika Sebuah Hayalan Tercapai"
+sleep 1
+echo "Kalian Jangan Lupa ingat kepDa tuhan:)"
+sleep 1
+echo "\033[32;1mIntiny Gue sayang Lo:)"
+sleep 1
+exit
+fi
